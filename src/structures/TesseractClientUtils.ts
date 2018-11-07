7cb7acffcd3115a8b3cf42ed9ad7646cc026d042
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-07 18:49:21 +0000

    structures: implement TesseractDataResolver in TesseractClientUtils

--- src/structures/TesseractClientUtils.ts
+++ src/structures/TesseractClientUtils.ts
@@ -1,12 +1,17 @@
 import * as discord from "discord.js";
 
+import TesseractDataResolver from "./TesseractDataResolver";
+
 // Utility class to help with common tasks.
 export default class TesseractClientUtils {
   client: discord.Client;
+  resolver: TesseractDataResolver;
 
   // TODO: Use TesseractClient
   constructor(client: discord.Client) {
     this.client = client;
+
+    this.resolver = new TesseractDataResolver(this.client);
   }
 
   // Fetches a user from Discord and then fetches their membership in a guild.
