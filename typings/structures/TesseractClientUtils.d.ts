a3a8035e420771a312f8f83147805bdc2a3d3403
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-07 16:48:10 +0000

    typings: add typings for TesseractClientUtils

---
+++ typings/structures/TesseractClientUtils.d.ts
@@ -0,0 +1,7 @@
+﻿import * as discord from "discord.js";
+export default class TesseractClientUtils {
+    client: discord.Client;
+    constructor(client: discord.Client);
+    fetchMember(guild: discord.Guild, id: discord.Snowflake, cache?: boolean): Promise<discord.GuildMember>;
+}
+//# sourceMappingURL=TesseractClientUtils.d.ts.map
