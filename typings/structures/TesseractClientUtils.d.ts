a6effd234b636e3efaf18deb4f1df9bf5c2e5423
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-07 19:28:51 +0000

    typings: update

--- typings/structures/TesseractClientUtils.d.ts
+++ typings/structures/TesseractClientUtils.d.ts
@@ -1,6 +1,8 @@
 ﻿import * as discord from "discord.js";
+import TesseractDataResolver from "./TesseractDataResolver";
 export default class TesseractClientUtils {
     client: discord.Client;
+    resolver: TesseractDataResolver;
     constructor(client: discord.Client);
     fetchMember(guild: discord.Guild, id: discord.Snowflake, cache?: boolean): Promise<discord.GuildMember>;
 }
