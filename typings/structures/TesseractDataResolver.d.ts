e888bef9655b407ecc12e6c73e82de733fc2e48d
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-08 16:10:11 +0000

    typings: update

--- typings/structures/TesseractDataResolver.d.ts
+++ typings/structures/TesseractDataResolver.d.ts
@@ -2,7 +2,7 @@
 export default class TesseractDataResolver {
     client: discord.Client;
     constructor(client: discord.Client);
-    resolveChannel(channel: discord.ChannelResolvable): discord.Channel;
+    resolveChannel(channel: discord.ChannelResolvable, channels: discord.Collection<discord.Snowflake, discord.Channel>): discord.Channel;
     resolveGuild(guild: discord.GuildResolvable): discord.Guild;
     resolveGuildMember(guild: discord.GuildResolvable, user: discord.UserResolvable): discord.GuildMember;
     resolveRole(guild: discord.GuildResolvable, role: discord.RoleResolvable): discord.Role;
