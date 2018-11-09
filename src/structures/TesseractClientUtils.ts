821cbb8d915b473a42911beef52dc074991e9cdf
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-09 10:46:45 +0000

    structures(TesseractClientUtils): add string compression/decompression methods

--- src/structures/TesseractClientUtils.ts
+++ src/structures/TesseractClientUtils.ts
@@ -1,4 +1,5 @@
 import * as discord from "discord.js";
+import * as lzutf8 from "lzutf8";
 
 import TesseractDataResolver from "./TesseractDataResolver";
 
@@ -12,6 +13,32 @@ export default class TesseractClientUtils {
     this.client = client;
 
     this.resolver = new TesseractDataResolver(this.client);
+  }
+
+  /**
+   * Compresses the given input string, with LZUTF8 encoding, and returns the
+   * compressed binary string.
+   */
+  compressString(string: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      lzutf8.compressAsync(string, { outputEncoding: "StorageBinaryString" }, (res: string, err: Error) => {
+        if (err) return reject(err);
+        return resolve(res);
+      });
+    })
+  }
+
+  /**
+   * Decompresses the given input BinaryString, encoded with LZUTF8 encoding,
+   * and returns the original string.
+   */
+  decompressString(string: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      lzutf8.decompressAsync(string, { inputEncoding: "StorageBinaryString" }, (res: string, err: Error) => {
+        if (err) return reject(err);
+        return resolve(res);
+      });
+    })
   }
 
   // Fetches a user from Discord and then fetches their membership in a guild.
