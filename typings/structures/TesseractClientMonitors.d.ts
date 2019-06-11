cd9ac9a4b24ac4e511727187d362873ea3ca61a5
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-12 04:15:02 +0530

    typings: delete all files

--- typings/structures/TesseractClientMonitors.d.ts
+++
@@ -1,7 +0,0 @@
-﻿import TesseractClient from "./TesseractClient";
-export default class TesseractClientMonitors {
-    client: TesseractClient;
-    constructor(client: TesseractClient);
-    exec(event: string, ...argv: any[]): void;
-}
-//# sourceMappingURL=TesseractClientMonitors.d.ts.map
