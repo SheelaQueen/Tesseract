317d2500935b101647e80da15f1a3a09f0167f91
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-13 08:00:50 +0530

    errors: add TesseractError base class

---
+++ src/errors/TesseractError.ts
@@ -0,0 +1,14 @@
+/**
+ * Base class for errors in Tesseract.
+ */
+class TesseractError extends Error {
+  constructor(message?: string) {
+    super(message);
+  }
+
+  public get name() : string {
+    return this.constructor.name;
+  }
+}
+
+export default TesseractError;
