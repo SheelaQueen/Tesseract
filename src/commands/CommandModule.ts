a28ead4230c21f7be492961227e8ec65a78821f2
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-24 23:56:10 +0530

    commands: add CommandModule

---
+++ src/commands/CommandModule.ts
@@ -0,0 +1,81 @@
+import { PermissionResolvable, Message } from "discord.js";
+import { Options as ArgumentParserOptions, Arguments as CommandArguments } from "yargs-parser";
+
+import TesseractModule from "../TesseractModule";
+import TesseractError from "../errors/TesseractError";
+
+interface ICommandModuleOptions {
+  /** The description of the command. */
+  description?: string;
+  /** The name and aliases for the command that can trigger this command. */
+  triggers?: string[];
+  /** Arguments configuration options for the arguments accepted by the command. */
+  arguments?: ArgumentParserOptions;
+  /** Execution scope of the command. Channel types where the command can be executed. */
+  scope?: "guild" | "dm";
+  /** Whether or not this command is only executable by the Tesseract client owner(s). */
+  owner?: boolean;
+  /** Whether or not to show 'typing...' status during command execution. */
+  typing?: boolean;
+  /** The cooldown period for the command (in milliseconds). */
+  cooldown?: number;
+  /** The number of times this command can be executed in the cooldown period. */
+  ratelimit?: number;
+  /** Permissions required by the Tesseract client to run this command. */
+  clientPermissions?: PermissionResolvable[];
+  /** Permissions required by the users to run this command. */
+  userPermissions?: PermissionResolvable[];
+  /** Any pre-run checks for the command. The command will execute only if this returns true. */
+  condition?: Function;
+}
+
+/**
+ * Abstract class for creating Tesseract client commands.
+ */
+abstract class CommandModule extends TesseractModule {
+  /** The description of the command. */
+  description: string;
+  /** The name and aliases for the command that can trigger this command. */
+  triggers: string[];
+  /** Arguments configuration options for the arguments accepted by the command. */
+  arguments: ArgumentParserOptions;
+  /** Execution scope of the command. Channel types where the command can be executed. */
+  scope: "guild" | "dm";
+  /** Whether or not this command is only executable by the Tesseract client owner(s). */
+  owner: boolean;
+  /** Whether or not to show 'typing...' status during command execution. */
+  typing: boolean;
+  /** The cooldown period for the command (in milliseconds). */
+  cooldown: number;
+  /** The number of times this command can be executed in the cooldown period. */
+  ratelimit: number;
+  /** Permissions required by the Tesseract client to run this command. */
+  clientPermissions: PermissionResolvable[];
+  /** Permissions required by the users to run this command. */
+  userPermissions: PermissionResolvable[];
+  /** Any pre-run checks for the command. The command will execute only if this returns true. */
+  condition: Function;
+
+  constructor(name: string, options: ICommandModuleOptions = {}) {
+    super(name);
+
+    this.name = name;
+    this.description = options.description;
+    this.triggers = options.triggers || [ name ];
+    this.arguments = options.arguments || {};
+    this.scope = options.scope;
+    this.owner = options.owner || false;
+    this.typing = options.typing || false;
+    this.cooldown = options.cooldown || 0;
+    this.ratelimit = options.ratelimit || 0;
+    this.clientPermissions = options.clientPermissions || [];
+    this.userPermissions = options.userPermissions || [];
+    this.condition = options.condition ? options.condition.bind(this) : () => true;
+  }
+
+  public exec(message: Message, argv: CommandArguments): Promise<any> {
+    throw new TesseractError(`Non-abstract class '${this.constructor.name}' does not implement inherited abstract method 'exec' from class 'CommandModule'`);
+  }
+}
+
+export default CommandModule;
