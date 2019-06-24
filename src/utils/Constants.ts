7f18486798b6000c921d9a4095f479dc7a0332e8
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-24 19:49:05 +0530

    utils(Constants): add ArgumentTypes

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -7,6 +7,35 @@
 
 import { Constants } from "discord.js";
 
+const ArgumentTypes = {
+  STRING: String,
+  UPPERCASE: (arg: string) => arg.toUpperCase(),
+  LOWERCASE: (arg: string) => arg.toLowerCase(),
+  NUMBER: (arg: string) => arg && !isNaN(Number(arg)) ? parseFloat(arg) : null,
+  INTEGER: (arg: string) => arg && !isNaN(Number(arg)) ? parseInt(arg) : null,
+  BIGINT: (arg: string) => arg && !isNaN(Number(arg)) ? BigInt(arg) : null,
+  URL: (arg: string) => {
+    try {
+      return new URL(arg);
+    } catch {
+      return null;
+    }
+  },
+  TIMESTAMP: (arg: string) => Date.parse(arg + "Z") || null,
+  DATE: (arg: string) => {
+    let timestamp: number = ArgumentTypes.TIMESTAMP(arg);
+    if (timestamp && !isNaN(timestamp)) return new Date(timestamp);
+    return null;
+  },
+  COLOR: (arg: string) => {
+    if (!arg) return null;
+
+    let color = parseInt(arg.replace("#", ""), 16);
+    if (color < 0 || color > 0xFFFFFF || isNaN(color)) return null;
+
+    return color;
+  },
+};
 
 enum LISTENER_MODE {
   /** It's invoked every time the specified event is triggered. */
@@ -30,6 +59,7 @@ let DiscordAPIErrors = Constants.APIErrors;
 
 
 export {
+  ArgumentTypes,
   LISTENER_MODE,
   DefaultOptions,
   ChannelTypes,
