b7650585f2bb387d36f4fec85b42e0ff5c148402
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-25 07:29:05 +0530

    typings: update

--- typings/TesseractModule.d.ts
+++ typings/TesseractModule.d.ts
@@ -4,8 +4,8 @@ import TesseractClient from "./client/TesseractClient";
 import TesseractModuleManager from "./TesseractModuleManager";
 declare abstract class TesseractModule extends EventEmitter {
     name: string;
-    abstract client: TesseractClient;
-    abstract manager: TesseractModuleManager;
+    client: TesseractClient;
+    manager: TesseractModuleManager;
     constructor(name: string);
     abstract exec(...args: any): Promise<any>;
     toString(): string;
