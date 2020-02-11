6bb48b7f24957816c82d70d2794dee508601e4fb
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-02-11 13:03:28 +0530

    interrupters: add InterruptModuleManager

---
+++ src/interrupters/InterruptModuleManager.ts
@@ -0,0 +1,15 @@
+import TesseractClient from "../client/TesseractClient";
+import TesseractModuleManager from "../TesseractModuleManager";
+
+/**
+ * Interrupt Module Manager loads interrupts.
+ */
+class InterruptModuleManager extends TesseractModuleManager {
+  constructor(client: TesseractClient) {
+    super(client, { directory: "./interrupts/" });
+
+    super.load();
+  }
+}
+
+export default InterruptModuleManager;
