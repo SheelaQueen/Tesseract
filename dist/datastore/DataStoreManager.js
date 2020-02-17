ab2434212d0467e9e878d018ac68499157b32987
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-02-17 21:14:36 +0530

    dist: update transpiled JS

---
+++ dist/datastore/DataStoreManager.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize = require("sequelize");
+const SQLiteProvider_1 = require("./providers/SQLiteProvider");
+;
+class DataStoreManager {
+    constructor(options) {
+        switch (options.dialect) {
+            case "sqlite":
+                this.provider = sequelize;
+                this.store = new SQLiteProvider_1.default(options.providerOptions);
+                break;
+        }
+    }
+}
+exports.default = DataStoreManager;
+;
+//# sourceMappingURL=DataStoreManager.js.map
---
+++ dist/datastore/providers/Provider.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Provider {
+}
+exports.default = Provider;
+;
+//# sourceMappingURL=Provider.js.map
