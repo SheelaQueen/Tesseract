a30affb14582def655cc7e60366ce8b90ef4c38b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-02-17 23:28:10 +0530

    dist: update transpiled JS

--- dist/datastore/DataStoreManager.js
+++ dist/datastore/DataStoreManager.js
@@ -1,15 +1,23 @@
 ﻿"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
 const sequelize = require("sequelize");
 const SQLiteProvider_1 = require("./providers/SQLiteProvider");
+const MongoProvider_1 = require("./providers/MongoProvider");
 ;
 class DataStoreManager {
     constructor(options) {
         switch (options.dialect) {
+            case "mongodb":
+                this.provider = mongoose;
+                this.store = new MongoProvider_1.default(options.providerOptions);
+                break;
             case "sqlite":
                 this.provider = sequelize;
                 this.store = new SQLiteProvider_1.default(options.providerOptions);
                 break;
+            default:
+                throw new Error("Tesseract DataStore dialect is invalid.");
         }
     }
 }
