90674367f2eaefc9ee979a5d43d9f76ee3e7464b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-02 20:00:43 +0530

    interrupters(InterruptModule): refactor
    * Remove useless options constructor parameter
    * Make exec method abstract

--- src/interrupters/InterruptModule.ts
+++ src/interrupters/InterruptModule.ts
@@ -1,22 +1,18 @@
 import TesseractModule from "../TesseractModule";
-import TesseractError from "../errors/TesseractError";
 
-interface IInterruptModuleOptions {
-}
 
 /**
  * Abstract class for creating Tesseract interrupts.
  */
 abstract class InterruptModule extends TesseractModule {
-  constructor(name: string, options: IInterruptModuleOptions = {}) {
+  constructor(name: string) {
     super(name);
 
     this.name = name;
   }
 
-  public exec(...args: any): Promise<boolean> {
-    throw new TesseractError(`Non-abstract class '${this.constructor.name}' does not implement inherited abstract method 'exec' from class 'InterruptModule'`);
-  }
+  public abstract exec(...args: any): Promise<boolean>;
 }
+
 
 export default InterruptModule;
