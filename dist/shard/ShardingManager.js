c954453d4daa888e378b5b549e3a370d8dc7682a
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-05 19:57:13 +0530

    dist: update transpiled JS
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ dist/shard/ShardingManager.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const settings = require("../utils/settings");
+class TesseractShardingManager extends discord_js_1.ShardingManager {
+    constructor(file, options) {
+        super(file, options);
+        const credentials = settings.getCredentials();
+        this.token = credentials.token;
+    }
+}
+exports.default = TesseractShardingManager;
+//# sourceMappingURL=ShardingManager.js.map
