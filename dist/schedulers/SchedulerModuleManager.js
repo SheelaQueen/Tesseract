26b1c2acf1c20503bf6d5125531b49776743bccf
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-10 23:41:24 +0530

    dist: update transpiled JS
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ dist/schedulers/SchedulerModuleManager.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cron = require("cron");
+const TesseractModuleManager_1 = require("../TesseractModuleManager");
+class SchedulerModuleManager extends TesseractModuleManager_1.default {
+    constructor(client) {
+        super(client, { directory: "./schedulers/" });
+        super.load();
+    }
+    initializeCronJob(scheduler) {
+        new cron.CronJob({
+            cronTime: scheduler.cronTime,
+            onTick: scheduler.exec,
+            start: true,
+            unrefTimeout: true,
+        });
+    }
+    initializeModule(scheduler) {
+        super.initializeModule(scheduler);
+        this.initializeCronJob(scheduler);
+        return scheduler;
+    }
+}
+exports.default = SchedulerModuleManager;
+//# sourceMappingURL=SchedulerModuleManager.js.map
