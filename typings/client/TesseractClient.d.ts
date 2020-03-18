12b6bf50fd8d1016e0126bbd5c4854c214756384
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-18 13:56:48 +0530

    typings: update
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- typings/client/TesseractClient.d.ts
+++ typings/client/TesseractClient.d.ts
@@ -3,6 +3,7 @@
  * @author Sankarsan Kampa (a.k.a. k3rn31p4nic)
  */
 import { Client, ClientOptions } from "discord.js";
+import TesseractClientLogger from "./TesseractClientLogger";
 import TesseractClientUtils from "./TesseractClientUtils";
 import DataStoreManager from "../datastore/DataStoreManager";
 import InterruptModuleManager from "../interrupters/InterruptModuleManager";
@@ -10,6 +11,7 @@ import * as settings from "../utils/settings";
 declare class TesseractClient extends Client {
     configurations: settings.TesseractConfigurations;
     credentials: settings.TesseractCredentials;
+    log: TesseractClientLogger;
     utils: TesseractClientUtils;
     interrupter: InterruptModuleManager;
     dataStore: DataStoreManager;
---
+++ typings/client/TesseractClientLogger.d.ts
@@ -0,0 +1,10 @@
+﻿import TesseractClient from "./TesseractClient";
+export default class TesseractClientLogger {
+    client: TesseractClient;
+    constructor(client: TesseractClient);
+    error: (...message: unknown[]) => void;
+    info: (...message: unknown[]) => void;
+    message: (...message: unknown[]) => void;
+    warn: (...message: unknown[]) => void;
+}
+//# sourceMappingURL=TesseractClientLogger.d.ts.map
