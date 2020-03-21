ae4a493cdec8ab415a6e6bbcaed5b4fad824fc84
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-21 15:19:59 +0530

    typings: update
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- typings/commands/CommandModuleManager.d.ts
+++ typings/commands/CommandModuleManager.d.ts
@@ -1,10 +1,15 @@
-﻿import TesseractModuleManager from "../TesseractModuleManager";
+﻿import { Snowflake } from "discord.js";
+import TesseractModuleManager from "../TesseractModuleManager";
 import TesseractClient from "../client/TesseractClient";
+import CommandModule from "./CommandModule";
 declare class CommandManager extends TesseractModuleManager {
     prefixes: string[];
+    triggers: Map<string, string>;
+    guildCommandUses: Map<Snowflake, Map<string, Map<Snowflake, number>>>;
     defaultCooldown: number;
     constructor(client: TesseractClient);
     private initialize;
+    protected storeModule(module: CommandModule): void;
     private handle;
     private parseCommandTrigger;
 }
