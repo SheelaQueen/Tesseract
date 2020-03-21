b6853c3c0eddfdc363daed071c32d538972c6dd6
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-21 15:19:45 +0530

    dist: update transpiled JS
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- dist/commands/CommandModule.js
+++ dist/commands/CommandModule.js
@@ -8,11 +8,10 @@ class CommandModule extends TesseractModule_1.default {
         this.description = options.description || "";
         this.triggers = options.triggers || [name];
         this.arguments = options.arguments || {};
-        this.scope = options.scope || "guild";
         this.owner = options.owner || false;
         this.typing = options.typing || false;
         this.cooldown = options.cooldown || 0;
-        this.ratelimit = options.ratelimit || 0;
+        this.ratelimit = options.ratelimit || 1;
         this.clientPermissions = options.clientPermissions || [];
         this.userPermissions = options.userPermissions || [];
         this.condition = options.condition ? options.condition.bind(this) : () => true;
--- dist/commands/CommandModuleManager.js
+++ dist/commands/CommandModuleManager.js
@@ -1,11 +1,14 @@
 ﻿"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const yargsParser = require("yargs-parser");
+const discord_js_1 = require("discord.js");
 const TesseractModuleManager_1 = require("../TesseractModuleManager");
 class CommandManager extends TesseractModuleManager_1.default {
     constructor(client) {
         super(client, { directory: "./commands/" });
         this.prefixes = client.configurations.prefixes;
+        this.triggers = new Map();
+        this.guildCommandUses = new Map();
         this.initialize();
         super.load();
     }
@@ -17,6 +20,12 @@ class CommandManager extends TesseractModuleManager_1.default {
                 this.handle(message);
             });
         });
+    }
+    storeModule(module) {
+        super.storeModule(module);
+        for (const trigger of module.triggers) {
+            this.triggers.set(trigger, module.name);
+        }
     }
     async handle(message) {
         for (const interrupt of this.client.interrupter.modules.array()) {
@@ -33,9 +42,25 @@ class CommandManager extends TesseractModuleManager_1.default {
         if (!commandTrigger) {
             return false;
         }
-        const command = this.modules.get(commandTrigger.command);
+        let command;
+        if (this.modules.has(commandTrigger.command)) {
+            command = this.modules.get(commandTrigger.command);
+        }
+        else if (this.triggers.has(commandTrigger.command)) {
+            command = this.modules.get(this.triggers.get(commandTrigger.command));
+        }
         if (!command) {
             return false;
+        }
+        switch (command.scope) {
+            case "guild":
+                if (!(message.channel instanceof discord_js_1.TextChannel))
+                    return;
+                break;
+            case "dm":
+                if (!(message.channel instanceof discord_js_1.DMChannel))
+                    return;
+                break;
         }
         if (command.owner && !this.client.credentials.owners.includes(message.author.id))
             return false;
@@ -47,9 +72,34 @@ class CommandManager extends TesseractModuleManager_1.default {
         }
         if (!command.condition())
             return false;
+        if (command.cooldown && command.ratelimit) {
+            if (!this.guildCommandUses.has(message.guild.id)) {
+                this.guildCommandUses.set(message.guild.id, new Map());
+            }
+            if (!this.guildCommandUses.get(message.guild.id).has(command.name)) {
+                this.guildCommandUses.get(message.guild.id).set(command.name, new Map());
+            }
+            let useCount = 0;
+            if (this.guildCommandUses.get(message.guild.id).get(command.name).has(message.author.id)) {
+                useCount = this.guildCommandUses.get(message.guild.id).get(command.name).get(message.author.id) || 0;
+            }
+            if (useCount >= command.ratelimit) {
+                return;
+            }
+            this.guildCommandUses.get(message.guild.id).get(command.name).set(message.author.id, useCount + 1);
+            this.client.setTimeout(() => {
+                this.guildCommandUses.get(message.guild.id).get(command.name).delete(message.author.id);
+            }, command.cooldown * 1000);
+        }
+        if (command.typing)
+            message.channel.startTyping().catch(() => {
+            });
         const parsedArguments = yargsParser(commandTrigger.arguments, command.arguments);
         parsedArguments._raw = commandTrigger.arguments;
         await command.exec(message, parsedArguments);
+        if (command.typing)
+            message.channel.startTyping().catch(() => {
+            });
         return true;
     }
     parseCommandTrigger(message, guildPrefixes = []) {
