cf08b8ce055089e73d4081b633e9b9e116eb59a0
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-30 17:39:38 +0530

    typings: update
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- typings/client/TesseractDataResolver.d.ts
+++ typings/client/TesseractDataResolver.d.ts
@@ -5,8 +5,12 @@ export default class TesseractDataResolver {
     constructor(client: TesseractClient);
     resolveGuild(guild: discord.GuildResolvable): discord.Guild;
     resolveGuildChannel(guild: discord.GuildResolvable, channel: discord.GuildChannelResolvable, types?: ("category" | "news" | "store" | "text" | "voice")[]): discord.GuildChannel;
+    resolveGuildChannels(guild: discord.GuildResolvable, channels: discord.GuildChannelResolvable[], types?: ("category" | "news" | "store" | "text" | "voice")[]): discord.GuildChannel[];
     resolveGuildMember(guild: discord.GuildResolvable, user: discord.UserResolvable): discord.GuildMember;
+    resolveGuildMembers(guild: discord.GuildResolvable, users: discord.UserResolvable[]): discord.GuildMember[];
     resolveRole(guild: discord.GuildResolvable, role: discord.RoleResolvable): discord.Role;
+    resolveRoles(guild: discord.GuildResolvable, roles: discord.RoleResolvable[]): discord.Role[];
     resolveUser(user: discord.UserResolvable): discord.User;
+    resolveUsers(users: discord.UserResolvable[]): discord.User[];
 }
 //# sourceMappingURL=TesseractDataResolver.d.ts.map
